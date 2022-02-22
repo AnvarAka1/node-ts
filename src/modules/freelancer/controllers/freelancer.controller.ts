e5a4afc2901b5f61da64a77e6d48082224b1b88a
Anvar <Anvar-abd_97@mail.ru>
2022-02-22 23:29:51 +0500

    feat: freelancer list and detail

---
+++ src/modules/freelancer/controllers/freelancer.controller.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express'
+
+import freelancerService from '../services/freelancer.service'
+
+const freelancerList = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const results = await freelancerService.freelancerList(req)
+    return res.status(200).json(results)
+  } catch (err) {
+    next(err)
+  }
+}
+const freelancerDetail = async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+  try {
+    const result = await freelancerService.freelancerDetail(req)
+    return res.status(200).json(result)
+  } catch (err) {
+    next(err)
+  }
+}
+
+export default {
+  freelancerList,
+  freelancerDetail
+}
--- src/modules/freelancer/routes/index.ts
+++ src/modules/freelancer/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 import profileController from '../controllers/profile.controller'
 import projectController from '../controllers/project.controller'
+import freelancerController from '../controllers/freelancer.controller'
 
 const router = Router()
 
@@ -9,8 +10,8 @@ router.get('/profile/detail', profileController.profileDetail)
 router.put('/profile/general/update', profileController.updateGeneral)
 router.put('/profile/security/update', profileController.updateSecurity)
 
-router.get('/freelancers/list', () => {})
-router.get('/freelancers/:id/detail', () => {})
+router.get('/freelancers/list', freelancerController.freelancerList)
+router.get('/freelancers/:id/detail', freelancerController.freelancerDetail)
 
 router.get('/projects/list', projectController.projectList)
 router.get('/projects/:id/detail', projectController.projectDetail)
